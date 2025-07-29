1e94fa8c54639c38f5c9613176760fde3c4c1d57
zowork <luolishu@zowalk.com>
2025-07-29 21:46:10 +0800

    fixed

--- dist/core/BaseSdkApi.d.ts
+++ dist/core/BaseSdkApi.d.ts
@@ -1,5 +1,5 @@
 import { BaseSdkClient } from '../http';
 export declare abstract class BaseSdkApi {
-    protected _client: BaseSdkClient | undefined;
+    protected _client: BaseSdkClient;
     constructor(client: BaseSdkClient);
 }
--- src/core/BaseSdkApi.ts
+++ src/core/BaseSdkApi.ts
@@ -1,7 +1,7 @@
 import { BaseSdkClient } from '../http';
 
 export abstract class BaseSdkApi {
-  protected _client: BaseSdkClient | undefined;
+  protected _client: BaseSdkClient;
 
   constructor(client: BaseSdkClient) {
     this._client = client;
