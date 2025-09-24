80812b710e71a1626b87e554d08682c5a2cbdc2b
zowork <luolishu@zowalk.com>
2025-09-24 19:09:24 +0800

    fixed

--- dist/core/BaseSdkService.d.ts
+++
@@ -1,5 +0,0 @@
-import { BaseSdkClient } from '../http';
-export declare abstract class BaseSdkService<T extends BaseSdkClient> {
-    protected _client: T;
-    constructor(client: T);
-}
--- dist/core/BaseSdkService.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BaseSdkService = void 0;
-class BaseSdkService {
-    constructor(client) {
-        this._client = client;
-    }
-}
-exports.BaseSdkService = BaseSdkService;
